ecd28a1fdfe82d441d5a99884a4e24472140e7f7
Olivier B. OURA <baudolivier.oura@gmail.com>
2021-05-11 16:08:22 +0000

    First commit

---
+++ src/main/java/com/biblio/bibliocrawling/BiblioCrawlingApplication.java
@@ -0,0 +1,13 @@
+package com.biblio.bibliocrawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioCrawlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioCrawlingApplication.class, args);
+	}
+
+}
